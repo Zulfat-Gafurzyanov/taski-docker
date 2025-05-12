77060ee0da4300046970b68111f75abd74708332
Zulfat-Gafurzyanov <gafurzyanov.zulfat@gmail.com>
2025-05-12 13:35:22 +1000

    на третье ревью

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'HOST_IP', 'DOMAIN']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 # Application definition
 
